1ae7ee624b9d1de70f4d75570f2a52aadc1f1ef7
Haiqa Qaiser <125520340+Haiqa461@users.noreply.github.com>
2023-05-14 09:51:36 +0500

    Add files via upload

---
+++ palindrom.py
@@ -0,0 +1,24 @@
+
+
+# a py code to determine whether a word is palindrome or not 
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    # Calling reverse function 
+    rev = reverse(s) 
+  
+    # Checking if both string are equal or not 
+    if (s == rev): 
+        return True
+    return False
+  
+  
+# Driver code 
+s = "malayalam"
+ans = isPalindrome(s) 
+  
+if ans == 1: 
+    print("Yes") 
+else: 
+    print("No")
